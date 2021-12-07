1089fe391d2d57f457ce404bc1b24df094bbb142
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-07 17:25:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var arr = [1, 10, 15, 23, 38];
-var add = 0;
-
-function res() {
-  for (var i = 0; i < arr.length; i++) {
-    add += arr[i];
-  }
-  return add;
+function media (n1, n2) {
+    var nota1 = n1;
+    var nota2 = n2;
+    m = (nota1 + nota2)/2;
+    return m;
 }
 
-var x = res();
-console.log(x)
 
+var resultado = media;
+var resultado2 = resultado(9, 6);
+console.log(resultado2);
+
+var total = media(6, 7);
+console.log(total);
