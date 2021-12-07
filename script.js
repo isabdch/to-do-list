456818c64fe717b6d654004525af5420ec8018d5
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-07 17:08:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-var nome = prompt("Qual é o nome do(a) aluno(a)?");
-var n1 = parseFloat(prompt("Qual é a 1ª nota de " + nome + "?"));
-var n2 = parseFloat(prompt("Qual é a 2ª nota de " + nome + "?"));
-var media = (n1 + n2)/2
-var posicao = ""
-if (media >= 8) {
-    posicao = "Ótimo";
-} else if (media >= 6.5) {
-    posicao = "Bom";
-} else if (media >= 1) {
-    posicao = "Regular";
+var arr = [1, 10, 15, 23, 38];
+var add = 0;
+
+function res() {
+  for (var i = 0; i < arr.length; i++) {
+    add += arr[i];
+  }
+  return add;
 }
-switch (posicao) {
-    case "Ótimo":
-        alert("A média é " + media + ".\nParabéns! " + nome + " é um(a) ótimo(a) aluno(a)!");
-        break
-    case "Bom":
-        alert("A média é " + media + ".\n" + nome + " é um(a) bom(a) aluno(a).");
-        break
-    case "Regular":
-        alert("A média é " + media + ".\n" + nome + " é um aluno(a) regular, precisa estudar mais!");     
-        break
-    default:
-        alert("Houve um erro. Por favor recarregue a página.");
-        break
-}
+
+var x = res();
+console.log(x)
+
