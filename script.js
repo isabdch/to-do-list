6f78ff15927d91d2e088279c2e876e4f8c2d621e
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-07 18:34:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var media = function(n1, n2) {
+var names = ["Isa", "Ana", "Thiago", "Vinicius"];
+var g1 = [7.0, 4.5, 6.0, 2.3];
+var g2 = [8.0, 9.0, 4.6, 6.0];
+
+function average(n1, n2) {
     return (n1 + n2)/2;
 }
-console.log(media(6, 9));
-// ou
-var m = media(6, 9);
-console.log(m);
+
+function position(avrg) {
+    if (avrg > 6) {
+        return "APPROVED";
+    } else {
+        return "FAILED";
+    }
+}
+
+for (var i in names) {
+    var grade1 = g1[i];
+    var grade2 = g2[i];
+    var avg = average(g1[i], g2[i]);
+    console.log(names[i] + " - " + grade1 + " - " + grade2 + " - " + avg + " - " + position(avg) + ".");
+}
+
