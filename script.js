3d5d1a7b207799ee5c85b4977f89aba0e8ffcad8
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-07 10:35:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var nome = prompt("Informe o nome do(a) aluno(a):");
-var nota1 = parseFloat(prompt("Informe a 1ª nota:"));
-var nota2 = parseFloat(prompt("Informe a 2ª nota:"));
-var media = (nota1 + nota2)/2
-if (media >= 7) {
-    alert("A média de " + nome + " é " + media + " e está acima de 7, portanto sua posição é de APROVADA.");
-} else if (media >= 3) {
-    alert("A média de " + nome + " é " + media + " e está entre 3 e 6.9, portanto sua posição é de RECUPERAÇÃO.");
-} else {
-    alert("A média de " + nome + " é " + media + " e está entre 0 e 2.9, portanto sua posição é REPROVADA.")
+var nome = prompt("Qual é o nome do(a) aluno(a)?");
+var n1 = parseFloat(prompt("Qual é a 1ª nota de " + nome + "?"));
+var n2 = parseFloat(prompt("Qual é a 2ª nota de " + nome + "?"));
+var media = (n1 + n2)/2
+var posicao = ""
+if (media >= 8) {
+    posicao = "Ótimo";
+} else if (media >= 6.5) {
+    posicao = "Bom";
+} else if (media >= 1) {
+    posicao = "Regular";
 }
-
-
+switch (posicao) {
+    case "Ótimo":
+        alert("A média é " + media + ".\nParabéns! " + nome + " é um(a) ótimo(a) aluno(a)!");
+        break
+    case "Bom":
+        alert("A média é " + media + ".\n" + nome + " é um(a) bom(a) aluno(a).");
+        break
+    case "Regular":
+        alert("A média é " + media + ".\n" + nome + " é um aluno(a) regular, precisa estudar mais!");     
+        break
+    default:
+        alert("Houve um erro. Por favor recarregue a página.");
+        break
+}
