cd5e411c9cba4dc9a1af75dd3f8b008ee8b6fa4a
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-08 09:26:36 -0300

    position character

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-var names = ["Isa", "Ana", "Thiago", "Vinicius"];
-var g1 = [7.0, 4.5, 6.0, 2.3];
-var g2 = [8.0, 9.0, 4.6, 6.0];
+var posicao = 56;
 
-function average(n1, n2) {
-    return (n1 + n2)/2;
-}
-
-function position(avrg) {
-    if (avrg >= 6) {
-        return "APPROVED";
-    } else {
-        return "FAILED";
+function position(a) {
+    if (a > 100) {
+        return 100;
+    } else if (a < 0) {
+        return 0;
+    } else if (a => 0 && a <= 100) {
+        return posicao;
     }
 }
 
-for (var i in names) {
-    var grade1 = g1[i];
-    var grade2 = g2[i];
-    var avg = average(g1[i], g2[i]);
-    console.log(names[i] + " - " + grade1 + " - " + grade2 + " - " + avg + " - " + position(avg) + ".");
-}
+console.log(position(posicao));
