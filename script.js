6249380248ebbc950f50c4bef28929315eab3e9f
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-09 08:27:33 -0300

    aluguel de carros
    usei objects, this e function

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-var alunos = {
-    nomes: ["Isa", "Ana", "Thiago"],
-    notas1: [8.6, 7, 4.5],
-    notas2: [7.5, 6.5, 8],
-    media: function(n1, n2) {
-        return (n1 + n2) / 2;
-    }
+//Calcular o valor total de um aluguel de carros.
+
+function total() {
+  if (this.idade >= 18) {
+    return (
+      " Você dirigiu por " +
+      this.dias +
+      " dias e andou " +
+      this.kms +
+      "Km/h. O total que você deve pagar é: R$" +
+      (parseFloat(this.dias * 10) + parseFloat(this.kms * 0.5) + parseFloat(90)) //Preço fixo de R$90 + R$10 por dia + R$0.50 por km andado.
+    ); 
+  } else if (this.idade < 18) {
+    return "Você ainda não tem permissão para dirigir. Volte quando for maior de idade.";
+  }
 }
 
-for (var i in alunos.nomes) {
-    console.log(alunos.nomes[i] + " - Nota 1: " + alunos.notas1[i] + " - Nota 2: " + alunos.notas2[i] + " - Média: " + alunos.media(alunos.notas1[i], alunos.notas2[i]));
-}
+var pessoa1 = {
+  nome: "João",
+  idade: 18,
+  dias: 15,
+  kms: 150,
+  totalS: total,
+};
+
+var pessoa2 = {
+  nome: "Ana",
+  idade: 16,
+  dias: 7,
+  kms: 34,
+  totalS: total,
+};
+
+console.log(pessoa1.totalS());
+console.log(pessoa2.totalS());
