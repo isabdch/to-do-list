0bdb52ad6a5fb0c90611ad69de52232e3c34188a
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-17 15:17:31 -0300

    comparando maneiras de criar um evento

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
-function load() {
-  let input1 = document.getElementsByTagName("input")[0];
-  let button = document.getElementsByTagName("button")[1];
-  let input2 = document.getElementsByTagName("input")[1];
-  let t1 = document.getElementsByTagName("h2")[0];
-  let t2 = document.getElementsByTagName("h2")[1];
-  input1.addEventListener("input", onInput);
-  button.addEventListener("click", hide_show);
-  input2.addEventListener("change", changeColor);
-  t1.addEventListener("click", maiusculas);
-  t2.addEventListener("click", minusculas);
+function f(e) {
+  console.log("Teste");
+  console.log(this);
+  console.log(e);
 }
 
-function onInput() {
-  let title = document.getElementsByTagName("h1")[0];
-  title.innerText = this.value;
+function f1(e) {
+  console.log("Outro teste");
+  console.log(this);
+  console.log(e);
+  console.log(e.target);
 }
 
-function changeColor() {
-  let title = document.getElementsByTagName("h1")[0];
-  title.style.color = this.value;
-}
-
-function hide_show() {
-  let title = document.getElementsByTagName("h1")[0];
-  if (title.style.display == "none") {
-    title.style.display = "inline-block";
-  } else {
-    title.style.display = "none";
-  }
-}
-
-function maiusculas() {
-  this.textContent = this.textContent.toUpperCase();
-}
-
-function minusculas() {
-  this.textContent = this.textContent.toLowerCase();
-}
+window.onload = function () {
+  let titulo = document.getElementsByTagName("h1")[0];
+  titulo.addEventListener("click", f1);
+};
