f201b849d909fe912ef0f00deebe6739b9fba5d3
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-28 14:24:31 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,13 +1,25 @@
-let button = document.getElementsByTagName("button")[0];
-let input = document.getElementsByTagName("input")[0];
-let ul = document.getElementsByTagName("ul")[0];
-let li = document.getElementsByTagName("li");
-let list = [];
+// get HTML elements.
+let input = document.querySelector("input"); // input.
+let addLi = document.querySelector(".addLi"); // add button.
+let clearLi = document.querySelector(".clearLi"); // clear button.
+let todoList = document.querySelector(".todo-list"); // ul list.
 
-button.addEventListener("click", addLi);
+// add events to the elements.
+addLi.addEventListener("click", addToDo);
+clearLi.addEventListener("click", clearToDo);
 
-function addLi() {
-  list.push(input.value);
+// events functions.
+function addToDo(event) {  
+  event.preventDefault();
+  let todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo-div");
+  let todoLi = document.createElement("li");
+  todoLi.classList.add("todo-li");
+  todoLi.innerText = input.value;
+  todoList.appendChild(todoDiv);
+  todoDiv.appendChild(todoLi);
 }
 
-console.log(list);
+function clearToDo(event) {  
+  event.preventDefault();
+}
