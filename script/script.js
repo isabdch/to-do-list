79611ca194c94087601403b20afbeeb3d68766ff
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-29 10:59:50 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,38 +1,68 @@
 // get HTML elements.
-let input = document.querySelector("input"); // input.
-let addLi = document.querySelector(".addLi"); // add button.
-let clearLi = document.querySelector(".clearLi"); // clear button.
-let todoList = document.querySelector(".todo-list"); // ul list.
+const input = document.querySelector("input"); // input.
+const addLi = document.querySelector(".addLi"); // add button.
+const clearLi = document.querySelector(".clearLi"); // clear button.
+const todoList = document.querySelector(".todo-list"); // ul list.
 
 // add events to the elements.
 addLi.addEventListener("click", addToDo);
 clearLi.addEventListener("click", clearToDo);
+todoList.addEventListener("click", removeAndCheck);
 
 // events functions.
 function addToDo(event) {
-  event.preventDefault();
+  event.preventDefault(); // to prevent that form buttons make their default behavior.
 
-  let todoDiv = document.createElement("div");
+  // creates new elements, then creates their classes names and connects them to other elements.
+  const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo-div");
-  todoList.appendChild(todoDiv);
 
-  let todoLi = document.createElement("li");
+  const todoLi = document.createElement("li");
   todoLi.classList.add("todo-li");
   todoDiv.appendChild(todoLi);
 
   todoLi.innerText = input.value;
+  input.value = "";
 
-  let removeLiButton = document.createElement("button");
-  removeLiButton.classList.add("remove-li");
+  const removeLiButton = document.createElement("button");
   removeLiButton.innerHTML = "<i class='fas fa-times'></i>";
-  todoDiv.appendChild(removeLiButton);
+  removeLiButton.classList.add("remove-li");
+  todoLi.appendChild(removeLiButton);
 
-  let completedButton = document.createElement("button");
-  completedButton.classList.add("completed-button");
-  completedButton.innerHTML = "<i class='fas fa-check'></i>";
-  todoDiv.appendChild(completedButton);
+  const checkedButton = document.createElement("button");
+  checkedButton.innerHTML = "<i class='fas fa-check'></i>";
+  checkedButton.classList.add("checked-button");
+  todoLi.appendChild(checkedButton);
+
+  todoList.appendChild(todoDiv);
 }
 
 function clearToDo(event) {
   event.preventDefault();
+  todoList.innerHTML = "";
+}
+
+function removeAndCheck(event) {
+  const item = event.target;
+  if (item.classList[0] === "remove-li") {
+    const toDo = item.parentElement;
+    toDo.remove();
+  }
+
+  if (item.classList[1] === "fa-times") {
+    const toDo = item.parentElement;
+    const toDo1 = toDo.parentElement;
+    toDo1.remove();
+  }
+
+  if (item.classList[0] === "checked-button") {
+    const toDo = item.parentElement;
+    toDo.classList.toggle("checked");
+  }
+
+  if (item.classList[1] === "fa-check") {
+    const toDo = item.parentElement;
+    const toDo1 = toDo.parentElement;
+    toDo1.classList.toggle("checked");
+  }
 }
