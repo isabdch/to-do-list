da2b00a04a565f15be829b4a8458c33826bccf36
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2021-12-29 17:54:37 -0300

    update

--- script/script.js
+++ script/script.js
@@ -4,11 +4,15 @@ const addLi = document.querySelector(".addLi"); // add button.
 const clearLi = document.querySelector(".clearLi"); // clear button.
 const todoList = document.querySelector(".todo-list"); // ul list.
 const header = document.querySelector("header");
+const filterOption = document.querySelector("select")[0];
+const li = document.querySelector("li");
 
 // add events to the elements.
+document.addEventListener("DOMContentLoaded", getToDo);
 addLi.addEventListener("click", addToDo);
 clearLi.addEventListener("click", clearToDo);
 todoList.addEventListener("click", removeAndCheck);
+filterOption.addEventListener("click", filterToDo);
 
 // events functions.
 function addToDo(event) {
@@ -25,6 +29,7 @@ function addToDo(event) {
   todoDiv.appendChild(todoLi);
 
   todoLi.innerText = input.value;
+  saveLocalToDo(input.value);
   input.value = "";
 
   const removeLiButton = document.createElement("button");
@@ -44,6 +49,7 @@ function clearToDo(event) {
   event.preventDefault();
   todoList.innerHTML = "";
   header.style.marginTop = "200px";
+  localStorage.clear();
 }
 
 function removeAndCheck(event) {
@@ -54,10 +60,100 @@ function removeAndCheck(event) {
     toDo.classList.add("fall");
     toDo.addEventListener("transitionend", () => {
       toDo.remove();
+      removeLocalStorage(toDo);
     });
   }
 
   if (item.classList[0] === "checked-button") {
     toDo.classList.toggle("checked");
   }
+}
+
+function filterToDo(event) {
+  const options = todoList.childNodes;
+
+  options.forEach((todo) => {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "checked":
+        if (todo.classList.contains("checked")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "unchecked":
+        if (!todo.classList.contains("checked")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function saveLocalToDo(todo) {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getToDo() {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.forEach(function (todo) {
+    header.style.marginTop = "50px";
+
+    // creates new elements, then creates their classes names and connects them to other elements.
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-div");
+
+    const todoLi = document.createElement("li");
+    todoLi.classList.add("todo-li");
+    todoDiv.appendChild(todoLi);
+
+    todoLi.innerText = todo;
+    input.value = "";
+
+    const removeLiButton = document.createElement("button");
+    removeLiButton.innerHTML = "<i class='fas fa-times'></i>";
+    removeLiButton.classList.add("remove-li");
+    todoLi.appendChild(removeLiButton);
+
+    const checkedButton = document.createElement("button");
+    checkedButton.innerHTML = "<i class='fas fa-check'></i>";
+    checkedButton.classList.add("checked-button");
+    todoLi.appendChild(checkedButton);
+
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalStorage(todo) {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const toDoIndex = todo.children[0].innerText;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  todos.splice(todos.indexOf(toDoIndex), 1);
 }
